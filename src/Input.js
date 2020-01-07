ad8b53c024e3de84cb53e5d039f25583b209cc13
rj1809 <rahuljindal1989@gmail.com>
2020-01-07 18:48:33 +0530

    input component useState hook test

---
+++ src/Input.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({secretWord}) => {
+    const [currentGuess, setCurrentGuess] = React.useState("");
+    return(
+        <div data-test="component-input">
+            <form className="form-inline">
+                <input
+                    type="text"
+                    placeholder="enter guess"
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    value={currentGuess}
+                    onChange={(event) => setCurrentGuess(event.target.value)}
+                />
+                <button 
+                    data-test="submit-button"
+                    className="btn btn-primary mb-2"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
+}
+
+Input.propTypes = {
+    secretWord: PropTypes.string.isRequired
+}
+
+export default Input;
